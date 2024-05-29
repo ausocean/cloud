b33b83078e2a80dbeeca34f055b0b65a614e9a2e
trekhopton <trek.hopton@gmail.com>
2024-05-29 14:13:14 +0930

    oceanbench/broadcast: remove templates from broadcast.js
    This was done because templates shouldn't be in js. They cause editors to complain about syntax errors.

--- cmd/oceanbench/s/broadcast.js
+++ cmd/oceanbench/s/broadcast.js
@@ -1,39 +1,45 @@
-function init() {
-    document.getElementById('time-zone').value = getTimezone();
-    {{if .CurrentBroadcast.StartTimeUnix}}sync('start-time', 'start-time-unix', 'time-zone', false);{{end}}
-    {{if .CurrentBroadcast.EndTimeUnix}}sync('end-time', 'end-time-unix', 'time-zone', false);{{end}}
+document.addEventListener('DOMContentLoaded', function() {
+  const startTimeUnix = document.getElementById('start-time-unix').dataset.startTimeUnix;
+  const endTimeUnix = document.getElementById('end-time-unix').dataset.endTimeUnix;
+  const sensorList = JSON.parse(document.getElementById('sensor-list').dataset.sensorList);
+  const sendMsg = document.getElementById('send-msg').dataset.sendMsg === 'true';
 
-    {{ range .CurrentBroadcast.SensorList }}
-      {{if eq .SendMsg true}}
-        document.getElementById("{{ .Name }}").checked = true;
-      {{end}}
-    {{ end }}
+  if (startTimeUnix) sync('start-time', 'start-time-unix', 'time-zone', false);
+  if (endTimeUnix) sync('end-time', 'end-time-unix', 'time-zone', false);
 
-    {{ if eq .CurrentBroadcast.SendMsg true}}
-      document.getElementById("report-sensor").checked = true;
-    {{end}}
+  sensorList.forEach(sensor => {
+    if (sensor.SendMsg) {
+      document.getElementById(sensor.Name).checked = true;
+    }
+  });
+
+  if (sendMsg) {
+    document.getElementById("report-sensor").checked = true;
   }
+});
 
-  function checkAll(form){
-    {{ range .CurrentBroadcast.SensorList }}
-      form.querySelector("input[id='{{ .Name }}']").checked = true;
-    {{ end }}
-    form.submit()
-  }
+function checkAll(form) {
+  const sensorList = JSON.parse(document.getElementById('sensor-list').dataset.sensorList);
+  sensorList.forEach(sensor => {
+    form.querySelector(`input[id='${sensor.Name}']`).checked = true;
+  });
+  form.submit();
+}
 
-  function uncheckAll(form){
-    {{ range .CurrentBroadcast.SensorList }}
-      form.querySelector("input[id='{{ .Name }}']").checked = false;
-    {{ end }}
-    form.submit()
-  }
+function uncheckAll(form) {
+  const sensorList = JSON.parse(document.getElementById('sensor-list').dataset.sensorList);
+  sensorList.forEach(sensor => {
+    form.querySelector(`input[id='${sensor.Name}']`).checked = false;
+  });
+  form.submit();
+}
 
-  function buttonClick(button){
-    button.form.querySelector("input[name='action']").value = button.value
-    button.form.submit()
-  }
+function buttonClick(button) {
+  button.form.querySelector("input[name='action']").value = button.value;
+  button.form.submit();
+}
 
-  function submitSelect(select){
-    select.form.querySelector("input[name='action']").value = "broadcast-select"
-    select.form.submit();
-  }
+function submitSelect(select) {
+  select.form.querySelector("input[name='action']").value = "broadcast-select";
+  select.form.submit();
+}
