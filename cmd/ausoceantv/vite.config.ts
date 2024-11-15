228a5366cc84a79c6d28edae4d3366ce232aa320
trekhopton <trek.hopton@gmail.com>
2024-11-15 16:12:24 +1030

    ausoceantv: deploy with vite to appengine
    This is done because initially we were thinking of serving front end files from go, now we'll as a static vite dist.

---
+++ cmd/ausoceantv/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                home: resolve(__dirname, 'home.html'),
+                watch: resolve(__dirname, 'watch.html'),
+            }
+        }
+    }
+})
