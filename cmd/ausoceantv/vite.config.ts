0a7d62621e2cbbb73e9f6cd986a4cbd68852290a
David (AusOcean) <davidsutton@ausocean.org>
2024-11-23 16:19:32 +1030

    AOTV: Run prettier format (#372)
    This change is purely a formatting change using the newly configured pretter plugin.

--- cmd/ausoceantv/vite.config.ts
+++ cmd/ausoceantv/vite.config.ts
@@ -1,23 +1,22 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
-
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                home: resolve(__dirname, 'home.html'),
-                watch: resolve(__dirname, 'watch.html'),
-            }
-        }
-    }, 
-    server: {
-        proxy: {
-            "/api": {
-                target: "http://localhost:8084",
-                changeOrigin: true,
-            },
-        },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        home: resolve(__dirname, "home.html"),
+        watch: resolve(__dirname, "watch.html"),
+      },
     },
-})
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8084",
+        changeOrigin: true,
+      },
+    },
+  },
+});
