4c0c959bb7fa90515a2d8886e3378d0c4a358cf0
David Sutton <david@ausocean.org>
2024-12-09 10:07:46 +1030

    AOTV: Add profile Page
    This change adds a profile page which is filled with data about
    the current user and their subscription.

---
+++ cmd/ausoceantv/webapp/src/registry/profile.ts
@@ -0,0 +1,2 @@
+export * from "../web-components/authenticator";
+export * from "../web-components/profile-details";
--- cmd/ausoceantv/webapp/src/utils/context.ts
+++ cmd/ausoceantv/webapp/src/utils/context.ts
@@ -1,4 +1,4 @@
 import type { User } from "../types/user";
 import { createContext } from "@lit/context";
 
-export const userContext = createContext<User | null>(Symbol("current-user"));
+export const userContext = createContext<User>(Symbol("current-user"));
