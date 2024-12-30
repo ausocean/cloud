5301338ca07d8d146a38f93840ec6bb642cfd3d1
David Sutton <david@ausocean.org>
2024-12-30 15:27:12 +1030

    AOTV: Support Stripe Subscriptions (#383)
    This change adds proper support for stripe subscriptions, as well as keeping support for once off purchases.

--- cmd/ausoceantv/stripe.go
+++ cmd/ausoceantv/stripe.go
@@ -32,12 +32,17 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
 	"github.com/stripe/stripe-go/v81"
+	"github.com/stripe/stripe-go/v81/customer"
 	"github.com/stripe/stripe-go/v81/paymentintent"
+	"github.com/stripe/stripe-go/v81/price"
+	"github.com/stripe/stripe-go/v81/subscription"
 
 	"github.com/ausocean/cloud/backend"
 	"github.com/ausocean/cloud/gauth"
 	"github.com/ausocean/cloud/model"
 )
+
+var ErrNoProductSelected = errors.New("no product selected")
 
 // setupStripe gets the secrets required to set the stripe Key.
 // The secrets required are DEV_STRIPE_SECRET_KEY for standalone mode,
@@ -89,9 +94,9 @@ func (svc *service) handleCreatePaymentIntent(c *fiber.Ctx) error {
 
 	ctx := context.Background()
 
-	_, err = model.GetSubscriberByEmail(ctx, svc.settingsStore, p.Email)
-	if !errors.Is(err, datastore.ErrNoSuchEntity) {
-		subscriber := &model.Subscriber{GivenName: p.GivenName, FamilyName: p.FamilyName, Email: p.Email}
+	subscriber, err := model.GetSubscriberByEmail(ctx, svc.settingsStore, p.Email)
+	if errors.Is(err, datastore.ErrNoSuchEntity) {
+		subscriber = &model.Subscriber{GivenName: p.GivenName, FamilyName: p.FamilyName, Email: p.Email}
 		err := model.CreateSubscriber(ctx, svc.settingsStore, subscriber)
 		if err != nil {
 			return fmt.Errorf("unable to create susbcriber %v: %w", subscriber, err)
@@ -100,34 +105,167 @@ func (svc *service) handleCreatePaymentIntent(c *fiber.Ctx) error {
 		return fmt.Errorf("failed getting subscriber by email: %w", err)
 	}
 
-	// TODO: Get product details.
-	// 	description := product.description
-	// 	price := calculatePrice(product)
+	customerID, err := svc.getCustomerID(subscriber)
+	if err != nil {
+		return fmt.Errorf("error getting customer ID: %w", err)
+	}
 
-	// Enable auto payment method for better conversions.
-	autoPaymentMethodEnabled := true
+	priceID := c.FormValue("priceID")
+	if priceID == "" {
+		err := c.SendStatus(fiber.StatusBadRequest)
+		if err != nil {
+			return fmt.Errorf("error sending status: %d, err: %w", fiber.StatusBadRequest, err)
+		}
+		return ErrNoProductSelected
+	}
 
-	// Create a PaymentIntent with amount and currency.
+	price, err := getPrice(priceID)
+	if err != nil {
+		return fmt.Errorf("error getting price: %w", err)
+	}
+
+	// If the price is recurring the selected product is a subscription and needs
+	// to be handled differently to the once off case.
+	if price.Recurring == nil {
+		return svc.createPaymentIntent(c, customerID, price)
+	}
+	return svc.createSubscriptionIntent(c, customerID, priceID)
+}
+
+type clientSecretResponse struct {
+	ClientSecret string `json:"clientSecret"`
+}
+
+func (svc *service) createPaymentIntent(c *fiber.Ctx, cid string, price *stripe.Price) error {
 	params := &stripe.PaymentIntentParams{
-		Amount:                  stripe.Int64(1099),
-		Currency:                stripe.String(string(stripe.CurrencyAUD)),
-		AutomaticPaymentMethods: &stripe.PaymentIntentAutomaticPaymentMethodsParams{Enabled: &autoPaymentMethodEnabled},
+		Amount:                  stripe.Int64(price.UnitAmount),
+		Currency:                (*string)(&price.Currency),
+		Customer:                stripe.String(cid),
+		AutomaticPaymentMethods: &stripe.PaymentIntentAutomaticPaymentMethodsParams{Enabled: stripe.Bool(true)},
+	}
+
+	// Check for an incomplete payment intent. This could happen if a user restarts the checkout flow,
+	// and Stripe recommends reusing payment intents to track the history of a purchase.
+	var err error
+	pi := getActivePaymentIntent(cid)
+	if pi != nil {
+		// Update the existing payment intent.
+		// Since customer and auto payment methods cannot be updated, they should be removed from the params.
+		params.Customer = nil
+		params.AutomaticPaymentMethods = nil
+		pi, err = paymentintent.Update(pi.ID, params)
+		if err != nil {
+			log.Errorf("error updating payment intent, will retry with new payment intent: %v", err)
+		} else {
+			return c.JSON(clientSecretResponse{pi.ClientSecret})
+		}
 	}
 
 	// NOTE: DO NOT LOG PAYMENT INTENT.
-	pi, err := paymentintent.New(params)
+	pi, err = paymentintent.New(params)
 	if err != nil {
-		log.Errorf("Error creating new Stripe payment intent: %v", err)
-		return c.App().ErrorHandler(c, fmt.Errorf("could not create payment intent: %w", err))
+		log.Errorf("error creating new Stripe payment intent: %v", err)
+		return fmt.Errorf("could not create payment intent: %w", err)
 	}
 
-	v := struct {
-		ClientSecret string `json:"clientSecret"`
-	}{
-		ClientSecret: pi.ClientSecret,
+	return c.JSON(clientSecretResponse{pi.ClientSecret})
+}
+
+func (svc *service) createSubscriptionIntent(c *fiber.Ctx, cid, pid string) error {
+	paymentSettings := &stripe.SubscriptionPaymentSettingsParams{
+		SaveDefaultPaymentMethod: stripe.String("on_subscription"),
+	}
+	subParams := &stripe.SubscriptionParams{
+		Customer: stripe.String(cid),
+		Items: []*stripe.SubscriptionItemsParams{{
+			Price: stripe.String(pid),
+		}},
+		PaymentSettings: paymentSettings,
+		PaymentBehavior: stripe.String("default_incomplete"),
+	}
+	subParams.AddExpand("latest_invoice.payment_intent")
+	s, err := subscription.New(subParams)
+	if err != nil {
+		return fmt.Errorf("unable to create new stripe subscription: %w", err)
 	}
 
-	return c.JSON(v)
+	return c.JSON(
+		struct {
+			SubscriptionID string `json:"subscriptionId"`
+			ClientSecret   string `json:"clientSecret"`
+		}{
+			SubscriptionID: s.ID,
+			ClientSecret:   s.LatestInvoice.PaymentIntent.ClientSecret,
+		},
+	)
+}
+
+// getActivePaymentIntent returns a payment intent for the user which does not have the
+// state succeeded or cancelled.
+func getActivePaymentIntent(cid string) *stripe.PaymentIntent {
+	// Get a payment intent.
+	intents := paymentintent.List(&stripe.PaymentIntentListParams{
+		Customer:   stripe.String(cid),
+		ListParams: stripe.ListParams{Limit: stripe.Int64(1)},
+	})
+
+	// Move the list to point to the returned payment intent.
+	if !intents.Next() {
+		return nil
+	}
+
+	pi := intents.PaymentIntent()
+	if pi.Status == stripe.PaymentIntentStatusSucceeded || pi.Status == stripe.PaymentIntentStatusCanceled {
+		return nil
+	}
+
+	return pi
+}
+
+// getCustomer returns the customer ID for the current user, if no customer exists, a
+// new customer is created.
+func (svc *service) getCustomerID(sub *model.Subscriber) (string, error) {
+	if sub.PaymentInfo != "" {
+		return sub.PaymentInfo, nil
+	}
+
+	id, err := createCustomer(sub.GivenName, sub.FamilyName, sub.Email)
+	if err != nil {
+		return "", fmt.Errorf("error creating new customer: %w", err)
+	}
+
+	sub.PaymentInfo = id
+	err = model.UpdateSubscriber(context.Background(), svc.settingsStore, sub)
+	if err != nil {
+		return "", fmt.Errorf("error updating subscriber with new payment info: %w", err)
+	}
+
+	return sub.PaymentInfo, nil
+}
+
+// createCustomer creates a new Stripe customer object, returning the ID of
+// the newly created customer.
+func createCustomer(givenName, familyName, email string) (string, error) {
+	// Set the parameters.
+	params := &stripe.CustomerParams{
+		Name:  stripe.String(fmt.Sprintf("%s %s", givenName, familyName)),
+		Email: stripe.String(email),
+	}
+
+	// Create a new customer.
+	cust, err := customer.New(params)
+	if err != nil {
+		return "", fmt.Errorf("error creating new stripe customer: %w", err)
+	}
+
+	return cust.ID, nil
+}
+
+// getPrice is a helper function to get the stripe price struct for a given
+// price ID (pid).
+func getPrice(pid string) (*stripe.Price, error) {
+	params := &stripe.PriceParams{}
+	return price.Get(pid, params)
 }
 
 func (svc *service) cancelSubscription(c *fiber.Ctx) error {
