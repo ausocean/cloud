84b515f7c24dfc1742354df21526c65a9441aacd
trekhopton <trek.hopton@gmail.com>
2025-04-09 15:26:18 +0930

    bench/s/broadcast.js: run prettier
    This was done to clean up code style.

--- cmd/oceanbench/s/broadcast.js
+++ cmd/oceanbench/s/broadcast.js
@@ -1,21 +1,21 @@
 var advancedOpts;
 var adv = false;
 
-let camSelect, conSelect
-let prevCamOn, prevCamShutdown, prevCamOff, prevConOn, prevConOff, prevURL
+let camSelect, conSelect;
+let prevCamOn, prevCamShutdown, prevCamOff, prevConOn, prevConOff, prevURL;
 
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('time-zone').value = getTimezone();
-  const startTimestamp = document.getElementById('start-timestamp').value;
-  const endTimestamp = document.getElementById('end-timestamp').value;
-  const sensorList = JSON.parse(document.getElementById('sensor-list').dataset.sensorList);
-  const sendMsg = document.getElementById('send-msg').dataset.sendMsg === 'true';
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("time-zone").value = getTimezone();
+  const startTimestamp = document.getElementById("start-timestamp").value;
+  const endTimestamp = document.getElementById("end-timestamp").value;
+  const sensorList = JSON.parse(document.getElementById("sensor-list").dataset.sensorList);
+  const sendMsg = document.getElementById("send-msg").dataset.sendMsg === "true";
 
-  if (startTimestamp) sync('start-time', 'start-timestamp', 'time-zone', false);
-  if (endTimestamp) sync('end-time', 'end-timestamp', 'time-zone', false);
+  if (startTimestamp) sync("start-time", "start-timestamp", "time-zone", false);
+  if (endTimestamp) sync("end-time", "end-timestamp", "time-zone", false);
 
-  if (sensorList){
-      sensorList.forEach(sensor => {
+  if (sensorList) {
+    sensorList.forEach((sensor) => {
       if (sensor.SendMsg) {
         document.getElementById(sensor.Name).checked = true;
       }
@@ -31,9 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
   // Read the cookie for the advanced settings.
   let cookies = document.cookie.split(";", 5);
   for (c of cookies) {
-    let cpair = c
-      .trim()
-      .split("=", 2);
+    let cpair = c.trim().split("=", 2);
     if (cpair[0] == "advanced") {
       adv = cpair[1] == "on" ? true : false;
     }
@@ -41,88 +39,88 @@ document.addEventListener('DOMContentLoaded', function() {
 
   advancedOpts = document.getElementsByClassName("advanced");
   for (opt of advancedOpts) {
-    adv ? document.getElementById("adv-options-toggle").checked = true : opt.style.display = "none";
+    adv ? (document.getElementById("adv-options-toggle").checked = true) : (opt.style.display = "none");
   }
 
-  camSelect = document.getElementById("camera-select")
-  conSelect = document.getElementById("controller-select")
+  camSelect = document.getElementById("camera-select");
+  conSelect = document.getElementById("controller-select");
 });
 
 function generateActions(e) {
-  let onActs = document.getElementById("on-actions")
-  let shutdownActs = document.getElementById("shutdown-actions")
-  let offActs = document.getElementById("off-actions")
-  let rtmpVar = document.getElementById("rtmp-var")
+  let onActs = document.getElementById("on-actions");
+  let shutdownActs = document.getElementById("shutdown-actions");
+  let offActs = document.getElementById("off-actions");
+  let rtmpVar = document.getElementById("rtmp-var");
 
-  let con = conSelect.options[conSelect.selectedIndex].value
-  let cam = camSelect.options[camSelect.selectedIndex].value
+  let con = conSelect.options[conSelect.selectedIndex].value;
+  let cam = camSelect.options[camSelect.selectedIndex].value;
 
-  console.log("controller:", con)
-  console.log("camera:", cam)
+  console.log("controller:", con);
+  console.log("camera:", cam);
 
-  let conOn = con.toLowerCase().replaceAll(":", "") + ".Power2=true"
-  let conOff = con.toLowerCase().replaceAll(":", "") + ".Power2=false"
-  let camOn = cam.toLowerCase().replaceAll(":", "") + ".mode=normal"
-  let camShutdown = cam.toLowerCase().replaceAll(":", "") + ".mode=shutdown"
-  let camOff = cam.toLowerCase().replaceAll(":", "") + ".mode=paused"
-  let url = cam.toLowerCase().replaceAll(":", "") + ".RTMPURL"
+  let conOn = con.toLowerCase().replaceAll(":", "") + ".Power2=true";
+  let conOff = con.toLowerCase().replaceAll(":", "") + ".Power2=false";
+  let camOn = cam.toLowerCase().replaceAll(":", "") + ".mode=normal";
+  let camShutdown = cam.toLowerCase().replaceAll(":", "") + ".mode=shutdown";
+  let camOff = cam.toLowerCase().replaceAll(":", "") + ".mode=paused";
+  let url = cam.toLowerCase().replaceAll(":", "") + ".RTMPURL";
 
   if (prevConOn != null && con != "Select") {
-    console.log("previous con")
-      onActs.value.replaceAll(prevConOn, conOn)
-      prevConOn = conOn
-      offActs.value.replaceAll(prevConOff, conOff)
-      prevConOff = conOff
+    console.log("previous con");
+    onActs.value.replaceAll(prevConOn, conOn);
+    prevConOn = conOn;
+    offActs.value.replaceAll(prevConOff, conOff);
+    prevConOff = conOff;
   } else if (con != "Select") {
-      onActs.value += conOn + ","
-      prevConOn = conOn
-      offActs.value += conOff + ","
-      prevConOff = conOff
+    onActs.value += conOn + ",";
+    prevConOn = conOn;
+    offActs.value += conOff + ",";
+    prevConOff = conOff;
   }
   if (prevCamOn != null && cam != "Select") {
-    onActs.value.replaceAll(prevCamOn, camOn)
-    prevCamOn = camOn
-    shutdownActs.value.replaceAll(prevCamShutdown, camShutdown)
-    prevCamShutdown = camShutdown
-    offActs.value.replaceAll(prevCamOff, camOff)
-    prevCamOff = camOff
-    rtmpVar.value.replaceAll(prevURL, url)
-    prevURL = url
+    onActs.value.replaceAll(prevCamOn, camOn);
+    prevCamOn = camOn;
+    shutdownActs.value.replaceAll(prevCamShutdown, camShutdown);
+    prevCamShutdown = camShutdown;
+    offActs.value.replaceAll(prevCamOff, camOff);
+    prevCamOff = camOff;
+    rtmpVar.value.replaceAll(prevURL, url);
+    prevURL = url;
   } else if (cam != "Select") {
-    onActs.value += camOn + ","
-    prevCamOn = camOn
-    shutdownActs.value += camShutdown + ","
-    prevCamShutdown = camShutdown
-    offActs.value += camOff + ","
-    prevCamOff = camOff
-    rtmpVar.value += url
-    prevURL = url
+    onActs.value += camOn + ",";
+    prevCamOn = camOn;
+    shutdownActs.value += camShutdown + ",";
+    prevCamShutdown = camShutdown;
+    offActs.value += camOff + ",";
+    prevCamOff = camOff;
+    rtmpVar.value += url;
+    prevURL = url;
   }
 }
 
 function handleSiteChange(event) {
-    // Make a request to change site.
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", "/api/set/site/"+event.detail["newSite"]);
-    xhr.onreadystatechange = ()=> {
-      if (xhr.readyState == XMLHttpRequest.DONE && xhr.responseText == "OK") {
-        location.assign("/admin/broadcast"); // This will empty the form.
-      }
+  // Make a request to change site.
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "/api/set/site/" + event.detail["newSite"]);
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == XMLHttpRequest.DONE && xhr.responseText == "OK") {
+      location.assign("/admin/broadcast"); // This will empty the form.
     }
-    xhr.send();
+  };
+  xhr.send();
 }
 
 function checkAll(form) {
-  const sensorList = JSON.parse(document.getElementById('sensor-list').dataset.sensorList);
-  sensorList.forEach(sensor => {
+  const sensorList = JSON.parse(document.getElementById("sensor-list").dataset.sensorList);
+  sensorList.forEach((sensor) => {
     form.querySelector(`input[id='${sensor.Name}']`).checked = true;
   });
   form.submit();
 }
 
 function uncheckAll(form) {
-  const sensorList = JSON.parse(document.getElementById('sensor-list').dataset.sensorList);
-  sensorList.forEach(sensor => {
+  const sensorList = JSON.parse(document.getElementById("sensor-list").dataset.sensorList);
+  sensorList.forEach((sensor) => {
     form.querySelector(`input[id='${sensor.Name}']`).checked = false;
   });
   form.submit();
@@ -134,7 +132,7 @@ function buttonClick(button) {
 }
 
 function submitSelect(select) {
-  if (!select){
+  if (!select) {
     select = document.getElementById("broadcast-select");
   }
   select.form.querySelector("input[name='action']").value = "broadcast-select";
@@ -143,7 +141,7 @@ function submitSelect(select) {
 
 function toggleAdvanced(checked) {
   for (opt of advancedOpts) {
-    checked ? opt.style.removeProperty("display") : opt.style.display = "none";
+    checked ? opt.style.removeProperty("display") : (opt.style.display = "none");
   }
 
   document.cookie = (checked ? "advanced=on;" : "advanced=off;") + " path=/admin/broadcast";
