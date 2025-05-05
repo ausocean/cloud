886bb921676a09d11bc55f094f1d02fbf5ebdce9
Saxon Nelson-Milton <saxon@ausocean.org>
2025-05-06 07:59:37 +0930

    otv: handle no voltage value when ESP off
    When we have a low voltage case, the ESP goes into alarm mode, but then
    there will be no voltage sensor value. So if we get a no such entity error, we
    assume the voltage is the alarm voltage.

--- cmd/oceantv/main.go
+++ cmd/oceantv/main.go
@@ -47,7 +47,7 @@ import (
 
 const (
 	projectID          = "oceantv"
-	version            = "v0.10.0"
+	version            = "v0.10.1"
 	projectURL         = "https://oceantv.appspot.com"
 	cronServiceAccount = "oceancron@appspot.gserviceaccount.com"
 	locationID         = "Australia/Adelaide" // TODO: Use site location.
