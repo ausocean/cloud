df7b3bcacf3bde64385687b33483feedfe36896a
trekhopton <trek.hopton@gmail.com>
2025-10-15 17:36:10 +1030

    ausoceantv: remove project directory
    This was done because it has moved to another repository.

--- cmd/ausoceantv/webapp/src/registry/profile.ts
+++
@@ -1,5 +0,0 @@
-// This type of registery file can be used to import multiple components at once. Sometimes this seems to be required when modules aren't working properly.
-
-export * from "../web-components/authenticator";
-export * from "../web-components/profile-details";
-export * from "../web-components/app-header";
--- cmd/ausoceantv/webapp/src/types/subscription.ts
+++
@@ -1,9 +0,0 @@
-export class Subscription {
-  SubscriberID: number = NaN;
-  FeedID: number = NaN;
-  Class: string = "";
-  Prefs: string = "";
-  Start: Date = new Date();
-  Finish: Date = new Date();
-  Renew: boolean = false;
-}
