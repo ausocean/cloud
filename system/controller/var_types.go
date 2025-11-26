dfdc1cef7fc47ae434c55c807c9cf7ba9f5fae91
David (AusOcean) <david@ausocean.org>
2025-11-27 09:46:28 +1030

    Define static vartypes for camera and controller.
    This change implements a VarType array for both a controller and a camera.
    This will allow for static variable type management, which will work even if a
    device has not yet reported.

---
+++ system/controller/var_types.go
@@ -0,0 +1,24 @@
+package controller
+
+import "github.com/ausocean/cloud/model"
+
+// VarTypes describes the types of the variables used by a controller.
+var VarTypes = []model.VarType{
+	{Name: "LogLevel", Type: model.VarTypeUint},
+	{Name: "Pulses", Type: model.VarTypeUint},
+	{Name: "PulseWidth", Type: model.VarTypeUint},
+	{Name: "PulseDutyCycle", Type: model.VarTypeUint},
+	{Name: "PulseCycle", Type: model.VarTypeUint},
+	{Name: "AutoRestart", Type: model.VarTypeUint},
+	{Name: "AlarmPeriod", Type: model.VarTypeUint},
+	{Name: "AlarmNetwork", Type: model.VarTypeUint},
+	{Name: "AlarmVoltage", Type: model.VarTypeUint},
+	{Name: "AlarmRecoveryVoltage", Type: model.VarTypeUint},
+	{Name: "PeakVoltage", Type: model.VarTypeUint},
+	{Name: "HeartbeatPeriod", Type: model.VarTypeUint},
+	{Name: "Power0", Type: model.VarTypeBool},
+	{Name: "Power1", Type: model.VarTypeBool},
+	{Name: "Power2", Type: model.VarTypeBool},
+	{Name: "Power3", Type: model.VarTypeBool},
+	{Name: "PulseSuppress", Type: model.VarTypeBool},
+}
