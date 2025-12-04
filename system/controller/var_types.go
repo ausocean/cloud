a007b6f7ec9a301df928120f03d059199fe49697
David (AusOcean) <david@ausocean.org>
2025-12-05 10:23:59 +1030

    System: return copy of vartypes slice
    This prevents the global var from being mutated.

--- system/controller/var_types.go
+++ system/controller/var_types.go
@@ -2,23 +2,25 @@ package controller
 
 import "github.com/ausocean/cloud/model"
 
-// VarTypes describes the types of the variables used by a controller.
-var VarTypes = []model.VarType{
-	{Name: "LogLevel", Type: model.VarTypeUint},
-	{Name: "Pulses", Type: model.VarTypeUint},
-	{Name: "PulseWidth", Type: model.VarTypeUint},
-	{Name: "PulseDutyCycle", Type: model.VarTypeUint},
-	{Name: "PulseCycle", Type: model.VarTypeUint},
-	{Name: "AutoRestart", Type: model.VarTypeUint},
-	{Name: "AlarmPeriod", Type: model.VarTypeUint},
-	{Name: "AlarmNetwork", Type: model.VarTypeUint},
-	{Name: "AlarmVoltage", Type: model.VarTypeUint},
-	{Name: "AlarmRecoveryVoltage", Type: model.VarTypeUint},
-	{Name: "PeakVoltage", Type: model.VarTypeUint},
-	{Name: "HeartbeatPeriod", Type: model.VarTypeUint},
-	{Name: "Power0", Type: model.VarTypeBool},
-	{Name: "Power1", Type: model.VarTypeBool},
-	{Name: "Power2", Type: model.VarTypeBool},
-	{Name: "Power3", Type: model.VarTypeBool},
-	{Name: "PulseSuppress", Type: model.VarTypeBool},
+// VarTypes returns the variable types for the controller.
+func VarTypes() []model.VarType {
+	return []model.VarType{
+		{Name: "LogLevel", Type: model.VarTypeUint},
+		{Name: "Pulses", Type: model.VarTypeUint},
+		{Name: "PulseWidth", Type: model.VarTypeUint},
+		{Name: "PulseDutyCycle", Type: model.VarTypeUint},
+		{Name: "PulseCycle", Type: model.VarTypeUint},
+		{Name: "AutoRestart", Type: model.VarTypeUint},
+		{Name: "AlarmPeriod", Type: model.VarTypeUint},
+		{Name: "AlarmNetwork", Type: model.VarTypeUint},
+		{Name: "AlarmVoltage", Type: model.VarTypeUint},
+		{Name: "AlarmRecoveryVoltage", Type: model.VarTypeUint},
+		{Name: "PeakVoltage", Type: model.VarTypeUint},
+		{Name: "HeartbeatPeriod", Type: model.VarTypeUint},
+		{Name: "Power0", Type: model.VarTypeBool},
+		{Name: "Power1", Type: model.VarTypeBool},
+		{Name: "Power2", Type: model.VarTypeBool},
+		{Name: "Power3", Type: model.VarTypeBool},
+		{Name: "PulseSuppress", Type: model.VarTypeBool},
+	}
 }
